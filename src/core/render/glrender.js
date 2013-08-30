c07cb77cb1bed057dca5cd6576521709b5af4e00
jiangtaogao <jiangtao.gao@autodesk.com>
2013-08-30 17:44:54 +0800

    create gl context in glrender

--- src/core/render/glrender.js
+++ src/core/render/glrender.js
@@ -3,5 +3,38 @@
  */
 
 // webgl render
+
+XIA.GLRender = function (canvas, attributes) {
+
+	canvas = canvas || document.createElement("canvas");
 	
+	if (typeof canvas == 'string') {
+		
+		canvas = document.getElementById(canvas);
+	}
+	
+    this.context = null;
+    var names = ["webgl", "experimental-webgl"];
+    for (var i = 0; i < names.length; ++i) {
+      try {
+        this.context = canvas.getContext(names[i], attributes);
+      } catch (e) {
+      }
+      if (this.context) {
+        break;
+      }
+    }
+    if (!this.context) {
+      testFailed("Unable to fetch WebGL rendering context for Canvas");
+    }	
+
+};
+
+
+XIA.GLRender.prototype = {
+
+	constructor: XIA.GLRender,
+
+};
+
 
